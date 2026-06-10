ac2d10b168b56f72613e7876a70f7f2795c803aa
mekinebr <153266053+mekinebr@users.noreply.github.com>
2026-06-10 10:49:58 -0400

    Update service worker cache version and optimize fetch

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,29 @@
-const CACHE='album-copa2026-v5';
+const CACHE='album-copa2026-v10';
 
 self.addEventListener('install', event => {
-event.waitUntil(
-caches.open(CACHE).then(cache => {
-return cache.addAll([
-'./',
-'./index.html',
-'./style.css',
-'./app.js',
-'./manifest.json'
-]);
-})
-);
-
-self.skipWaiting();
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-event.waitUntil(
-caches.keys().then(keys =>
-Promise.all(
-keys.map(key => {
-if (key !== CACHE) {
-return caches.delete(key);
-}
-})
-)
-)
-);
-
-self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-event.respondWith(
-fetch(event.request)
-.then(response => {
-const copy = response.clone();
-
-```
-    caches.open(CACHE).then(cache => {
-      cache.put(event.request, copy);
-    });
-
-    return response;
-  })
-  .catch(() => caches.match(event.request))
-```
-
-);
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
