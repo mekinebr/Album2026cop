6cd96d2387a0a0963d18d76ba8e0c90ffcf86e03
mekinebr <153266053+mekinebr@users.noreply.github.com>
2026-06-10 10:23:47 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,52 @@
-const CACHE='album-copa2026-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.json'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE='album-copa2026-v5';
+
+self.addEventListener('install', event => {
+event.waitUntil(
+caches.open(CACHE).then(cache => {
+return cache.addAll([
+'./',
+'./index.html',
+'./style.css',
+'./app.js',
+'./manifest.json'
+]);
+})
+);
+
+self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys.map(key => {
+if (key !== CACHE) {
+return caches.delete(key);
+}
+})
+)
+)
+);
+
+self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+event.respondWith(
+fetch(event.request)
+.then(response => {
+const copy = response.clone();
+
+```
+    caches.open(CACHE).then(cache => {
+      cache.put(event.request, copy);
+    });
+
+    return response;
+  })
+  .catch(() => caches.match(event.request))
+```
+
+);
+});
